d15a859b9f564670d0b3d7da25bac31700d97dca
Nelson Campos <nelsoncamposcapa@gmail.com>
2023-03-16 18:02:25 -0300

    Se agrega configuracion

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    env:{
+      snapshotOnly: true,
+      requestMode: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
